e6b77a5bbc09f736eeaa24f2d322672317684ba6
703003004 <ethan.rcolon@gmail.com>
2024-09-27 17:17:23 +0000

    Main

--- Stapler.java
+++ Stapler.java
@@ -1,53 +1,71 @@
 //Edit only code in this file
 
+
 class Stapler{
     //Data
     private int staples;
     private boolean isOpen;
-		private int capacity;
-    
+        private int capacity = 500;
+   
     //Methods (Behaviors)
     public Stapler(){
         staples = 0;
         isOpen = false;
     }
-    
+   
     public void loadStaples(int s){
         //Update staples variable to be s more, but no more than the total capacity (default is 500).
         //If s is not positive, ignore the value
-        
-        
+        if(s>0){
+            staples=staples+s;
+            if(staples>capacity){
+                staples=capacity;
+            }
+
+
+        }
+       
     }
-    
     public boolean staple(){
         //1.  If the stapler is closed and not out of staples, staple!
         //2.  Reduce the number of staples by 1 & return true.
         //If there was a problem stapling (not closed, out of staples) return false;
-        
+        if(isOpen==false){
+            if(staples<1){
+                return false;
+            }
+            else{
+                staples--;
+                return true;
+            }
+        }
         return false;
     }
-    
+   
     public void open(){
         //Open the stapler
-        
+        isOpen=true;
     }
-    
+   
     public void close(){
         //Close the stapler;
-        
+        isOpen=false;
     }
-    
+   
     public int getStaples(){
         return staples;
     }
-    
+   
     public boolean isClosed(){
         return !isOpen;
     }
-		/** Method to get print information about the Stapler
-	@return returns a phrase like "This stapler has room for x staples" where x is the remaining capacity after accounting for staples in the Stapler
-	**/
-		public String toString(){
-			return "";
-		}
+        /** Method to get print information about the Stapler
+    @return returns a phrase like "This stapler has room for x staples" where x is the remaining capacity after accounting for staples in the Stapler
+    **/
+        public String toString(){
+            return "This stapler has room for " + (capacity - staples) + " staples";
+        }
 }
+
+
+
